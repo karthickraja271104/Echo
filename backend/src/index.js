c6752af1a20d893cdbb12f023778f6592c610914
karthickraja271104 <karthickraja66812@gmail.com>
2025-10-03 21:03:02 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,9 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));           // for JSON payloads
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // for URL-encoded payloads
+
 app.use(cookieParser());
 app.use(
   cors({
