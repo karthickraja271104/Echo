dea1b07619e6714a15f5ffb90f5a6ab7f865dcd7
karthickraja271104 <karthickraja66812@gmail.com>
2026-01-29 10:14:29 +0530

    Include replyTo in sendMessage function
    Added 'replyTo' field to message body and message save operation.

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -37,13 +37,12 @@ export const getMessages = async (req, res) => {
 
 export const sendMessage = async (req, res) => {
   try {
-    const { text, image } = req.body;
+    const { text, image, replyTo } = req.body;
     const { id: receiverId } = req.params;
     const senderId = req.user._id;
 
     let imageUrl;
     if (image) {
-      // Upload base64 image to cloudinary
       const uploadResponse = await cloudinary.uploader.upload(image);
       imageUrl = uploadResponse.secure_url;
     }
@@ -53,6 +52,7 @@ export const sendMessage = async (req, res) => {
       receiverId,
       text,
       image: imageUrl,
+      replyTo: replyTo || null,  // Add this line
     });
 
     await newMessage.save();
