70eb42ef7fcbb5609e6fc9b5ee22d77d10a07862
karthickraja271104 <karthickraja66812@gmail.com>
2026-01-29 10:12:42 +0530

    Add replyTo field to message model

--- backend/src/models/message.model.js
+++ backend/src/models/message.model.js
@@ -18,6 +18,11 @@ const messageSchema = new mongoose.Schema(
     image: {
       type: String,
     },
+    replyTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Message",
+      default: null,
+    },
   },
   { timestamps: true }
 );
