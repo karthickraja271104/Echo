f9603a0b0881650d90009f47c56dd9fd6356052a
karthickraja271104 <karthickraja66812@gmail.com>
2026-01-29 10:03:28 +0530

    Change formatMessageTime to use locale string

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,7 +1,9 @@
 export function formatMessageTime(date) {
-  return new Date(date).toLocaleTimeString("en-US", {
+  return new Date(date).toLocaleString("en-US", {
+    month: "short",
+    day: "numeric",
     hour: "2-digit",
     minute: "2-digit",
-    hour12: false,
+    hour12: true,
   });
 }
